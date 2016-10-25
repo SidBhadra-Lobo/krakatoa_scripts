fbd949fec9c463e20dcc9846b620689961ece6f2
SidBhadra-Lobo <sbhadralobo@ucdavis.edu>
2016-10-25 17:37:07 -0400

    reading all csv in dir into hash

--- nextseq_samplesheet_builder.pl
+++ nextseq_samplesheet_builder.pl
@@ -5,25 +5,33 @@
 use strict;
 use warnings;
 
+my $dir = '/home/sbhadral/barcode_tables';
+
+foreach my $fp (glob("$dir/*.csv")) {
+    printf "%s\n", $fp;
+
+
 # open .csv table.
-open (my $fh_brcd, "<", "/home/sbhadral/barcode_tables/truseq_dna_meth_barcodes.csv")
-    or die "Can't open < your_barcode.csv: $!";
+open (my $fh_brcd, "<", $fp)
+    or die "Can't open < dir/your_barcode.csv: $!";
 
-my %truseq; #initialize hash table 
-while (my $line=<$fh_brcd>) { # read lines in
-    if($line !~ /IndexName/) {
-    chomp $line;
-    print $line,"\n";
+    my %truseq; #initialize hash table 
+    while (my $line=<$fh_brcd>) { # read lines in
+        if($line !~ /IndexName/) { #ignores the header
+        chomp $line;
+        print $line,"\n";
 
-    my ($word1, $word2) = split /,/, $line; #split index and seq by ','
+        my ($word1, $word2) = split /,/, $line; #split index and seq by ','
 
-    $truseq{$word1} = $word2; # assign val to key.
+        $truseq{$word1} = $word2; # assign val to key.
+        }
     }
-}
 
-while ( my ($key, $val) = each %truseq) { #check if keys are assigned correctly.
-    print "Key $key => $val\n"
-}
+    while ( my ($key, $val) = each %truseq) { #check if keys are assigned correctly.
+        print "Key $key => $val\n"
+        }
 
 
 close($fh_brcd) || warn "close failed: $!";
+
+}
