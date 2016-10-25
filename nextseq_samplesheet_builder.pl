1b061c46f6af7c12345144cca790c1c7118b44c1
SidBhadra-Lobo <sbhadralobo@ucdavis.edu>
2016-10-25 19:32:58 -0400

    building csv

--- nextseq_samplesheet_builder.pl
+++ nextseq_samplesheet_builder.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-#Nextseq SampleSheet.csv Builder.
+#Nextseq SampleSheet.csv builder and barcode validator/generator.
 
 use strict;
 use warnings;
@@ -16,14 +16,14 @@ while (my $line=<$fh_brcd>) { # read lines in
     chomp $line;
     print $line,"\n";
 
-    my ($word1, $word2) = split /,/, $line; #split index and seq by ','
+    my ($index, $seq) = split /,/, $line; #split index and seq by ','
 
-    $barcodes{$word1} = $word2; # assign val to key.
+    $barcodes{$index} = $seq; # assign val to key.
     }
 }
 
-    while ( my ($key, $seq) = each %barcodes) { #check if keys are assigned correctly.
-        print "Key $key => $seq\n"
+    while ( my ($key, $val) = each %barcodes) { #check if keys are assigned correctly.
+        print "Key $key => $val\n"
     }
 
 #Function to check single barcodes entered.
@@ -33,16 +33,17 @@ print "Enter a barcode sequence to check : \n";
 
 chomp(my $brcd_seq = <STDIN>);
 
-    print "You entered $brcd_seq.\n";
+    # print "You entered $brcd_seq.\n";
 
-        my %revhash = reverse %barcodes;
+        my %revhash = reverse %barcodes; #get keys from value using reversed hash.
 
-    if( exists $revhash{$brcd_seq} ) {
+    if( exists $revhash{$brcd_seq} ) { # check barcode give index if valid.
+        
         print "Your barcode sequence is valid.\n";
         print "Here is the barcode index: ";
         # my %revhash = reverse %barcodes;
-        my $key = $revhash{$brcd_seq};
-        print $key."\n"; 
+        my $key_check = $revhash{$brcd_seq};
+        print $key_check."\n"; 
     }
     if(length($brcd_seq) < 6) {
         print "Your barcode seems too short (less than 5 bp): $brcd_seq\n";
@@ -52,15 +53,27 @@ chomp(my $brcd_seq = <STDIN>);
     }
 
 
+#Start to build csv, trying to use Class::CSV.
+# use Class::CSV;
+
+# my $csv = Class::CSV->new(
+#   fields         => [qw/Sample_ID Sample_Name Sample_Plate Sample_Well I7_Index_ID index Sample_Project Description/]
+# );
+
+# $csv->add_line(['sample1', 'sample1name', 'test', 'test', 'A044', 'TATAAT', 'project1', 'description1']);
+# $csv->add_line({
+#   Sample_ID   => 'sample2',
+#   Sample_Name => 'sample2name'
+# });
+
+# $csv->print();
+
 # Check for duplicate barcodes when reading in multiple barcodes from list.
 # my @keys = grep { $barcode{$_} eq $val } keys %barcodes; #check for duplicates
 
 close($fh_brcd) || warn "close failed: $!";
 
 #Get all csv files in a dir read into a hash, approach.
-
-# use strict;
-# use warnings;
 
 # my $dir = '/home/sbhadral/barcode_tables'; #set up the dir with csv tables.
 
@@ -70,23 +83,6 @@ close($fh_brcd) || warn "close failed: $!";
 # # open .csv table.
 # open (my $fh_brcd, "<", $fp)
 #     or die "Can't open < dir/your_barcode.csv: $!";
-
-#     my %truseq; #initialize hash table 
-#     while (my $line=<$fh_brcd>) { # read lines in
-#         if($line !~ /IndexName/) { #ignores the header
-#         chomp $line;
-#         print $line,"\n";
-
-#         my ($word1, $word2) = split /,/, $line; #split index and seq by ','
-
-#         $truseq{$word1} = $word2; # assign val to key.
-#         }
-#     }
-
-#     while ( my ($key, $val) = each %truseq) { #check if keys are assigned correctly.
-#         print "Key $key => $val\n"
-#         }
-
 
 # close($fh_brcd) || warn "close failed: $!";
 
