2b27ab1cb0806a5b9a00821bbac4a9732ed91f2e
SidBhadra-Lobo <sbhadralobo@ucdavis.edu>
2016-10-25 15:41:57 -0400

    updated to include *hs_mt*L001*R1 files

--- nextseq_processing_alignment_pipeline.sh
+++ nextseq_processing_alignment_pipeline.sh
@@ -14,12 +14,15 @@ fi
 echo "Enter the desired run number (4 digits, like 0001), then press [ENTER]: ";
 read run_number;
 
-#write info in script to logfile.
-echo "$run_number" >> /git/nextseq/processed/"$dir_name".logfile;
-echo $d >> /git/nextseq/processed/"$dir_name".logfile;
-
-echo "Enter '1' if want to convert trim and convert the fastqs->fasta, if not enter '0', then press [ENTER]: ";
+echo "Enter '1' if want to convert the fastqs->fasta, if not enter '0', then press [ENTER]: ";
 read convert;
+
+echo "Enter '1' if want trim the fastqs->fasta, if not enter '0', then press [ENTER]: ";
+read trunc;
+
+#write info in script to logfile.
+echo run number: "$run_number" converted: "$convert" truncated: "$trunc" >> /git/nextseq/processed/"$dir_name".logfile;
+echo $d >> /git/nextseq/processed/"$dir_name".logfile;
 
 run=$(echo /git/nextseq/*_"$run_number"_*); #Change this to the path of desired sequencing run.
 
@@ -29,16 +32,19 @@ dir_name=$(echo $run | sed 's/.*nextseq\///' | cut -f 1,3 -d_ );
 
 d=$(date);
 
-echo Starting demultiplexing. >> /git/nextseq/processed/"$dir_name".logfile;
-echo $d >> /git/nextseq/processed/"$dir_name".logfile;
+####COMMENT OUT IF YOU WANT TO ONLY DO CONVERSION######
+# echo Starting demultiplexing. >> /git/nextseq/processed/"$dir_name".logfile;
+# echo $d >> /git/nextseq/processed/"$dir_name".logfile;
 
 
-#Run illumina's bcl2fastq conversion tool. 
-# -R for location of your run. nohup*.out is your bcl2fastq log file, '>' chooses a save location.
-nohup /usr/local/bin/bcl2fastq -R "$run" -o /git/nextseq/processed/"$dir_name" -r 16 -d 16 -p 16 -w "$sample_count" > /git/nextseq/processed/nohup_"$dir_name".out 2>&1 &
+# #Run illumina's bcl2fastq conversion tool. 
+# # -R for location of your run. nohup*.out is your bcl2fastq log file, '>' chooses a save location.
+# nohup /usr/local/bin/bcl2fastq -R "$run" -o /git/nextseq/processed/"$dir_name" -r 16 -d 16 -p 16 -w "$sample_count" > /git/nextseq/processed/nohup_"$dir_name".out 2>&1 &
 
-#wait for demultiplex to finish before conversion.
-wait;
+# #wait for demultiplex to finish before conversion.
+# wait;
+####COMMENT OUT IF YOU WANT TO ONLY DO CONVERSION######
+
 
 #If the user does not want conversion and truncation of fastq.gz files.
 if [[ "$convert" == 0 ]]; then 
@@ -56,20 +62,34 @@ then
     echo $d >> /git/nextseq/processed/"$dir_name".logfile;
 
     #Get all Lane 1 Read 1 fastq files, not including "undetermined"
-    ls /git/nextseq/processed/"$dir_name"/*_*/*L001*R1*.gz > /git/nextseq/processed/"$dir_name"/L1R1_"$dir_name"_list.txt;
+    ls /git/nextseq/processed/"$dir_name"/*_*/*hs_mt*L001*R1*.gz > /git/nextseq/processed/"$dir_name"/L1R1_"$dir_name"_list.txt;
 
 
     #loop through all L001_R1 files generated from demultiplexing.
     for fastq_file in $(less /git/nextseq/processed/"$dir_name"/L1R1_"$dir_name"_list.txt);
 
     do
+
         #get "name" for input in nextseq_gzip_mrg.pl
         name=$(echo "$fastq_file" | sed 's/_L001_R1_001.fastq.gz//' | sed 's/.*\///');
 
-        #run nextseq_gzip_mrg.pl 
-        /usr/bin/perl /home/sbhadral/scripts/nextseq_gzip_mrg.pl "$fastq_file" "$name" DNA 1 >> /git/nextseq/processed/"$dir_name".logfile 2>&1 &
+        #If the user does not want to truncate.
+        if [[ "$trunc" == 0 ]]; then 
+
+            #run nextseq_gzip_mrg.pl without truncation.
+            /usr/bin/perl /home/sbhadral/scripts/nextseq_gzip_mrg.pl "$fastq_file" "$name" DNA "$trunc" >> /git/nextseq/processed/"$dir_name".logfile 2>&1 &
 
 
+        fi;
+
+        #If the user wants to truncate.
+        if [[ "$trunc" == 1 ]]; then
+            
+            #run nextseq_gzip_mrg.pl with truncation.
+            /usr/bin/perl /home/sbhadral/scripts/nextseq_gzip_mrg.pl "$fastq_file" "$name" DNA "$trunc" >> /git/nextseq/processed/"$dir_name".logfile 2>&1 &
+
+        fi;
+        
     done;
 
 fi;
