5b9c28fd86219c67c050e94a101a36dfd1b12eb2
SidBhadra-Lobo <sbhadralobo@ucdavis.edu>
2016-10-26 16:30:25 -0400

    can choose to demultiplex or not if already done

--- nextseq_processing_alignment_pipeline.sh
+++ nextseq_processing_alignment_pipeline.sh
@@ -14,14 +14,17 @@ fi
 echo "Enter the desired run number (4 digits, like 0001), then press [ENTER]: ";
 read run_number;
 
-echo "Enter '1' if want to convert the fastqs->fasta, if not enter '0', then press [ENTER]: ";
+echo "Enter '1' if you want to demultiplex this run?, if not (you already demultiplexed) enter '0', then press [ENTER]: ";
+read demul;
+
+echo "Enter '1' if you want to convert the fastqs->fasta, if not enter '0', then press [ENTER]: ";
 read convert;
 
-echo "Enter '1' if want trim the fastqs->fasta, if not enter '0', then press [ENTER]: ";
+echo "Enter '1' if you want trim the fastqs->fasta, if not enter '0', then press [ENTER]: ";
 read trunc;
 
 #write info in script to logfile.
-echo run number: "$run_number" converted: "$convert" truncated: "$trunc" >> /git/nextseq/processed/"$dir_name".logfile;
+echo run number: "$run_number" demultiplexed: "$demul" converted: "$convert" truncated: "$trunc" >> /git/nextseq/processed/"$dir_name".logfile;
 echo $d >> /git/nextseq/processed/"$dir_name".logfile;
 
 run=$(echo /git/nextseq/*_"$run_number"_*); #Change this to the path of desired sequencing run.
@@ -33,18 +36,27 @@ dir_name=$(echo $run | sed 's/.*nextseq\///' | cut -f 1,3 -d_ );
 d=$(date);
 
 ###COMMENT OUT IF YOU WANT TO ONLY DO CONVERSION######
+if [[ "$demul" == 1 ]]; then
+
 echo Starting demultiplexing. >> /git/nextseq/processed/"$dir_name".logfile;
 echo $d >> /git/nextseq/processed/"$dir_name".logfile;
-
 
 #Run illumina's bcl2fastq conversion tool. 
 # -R for location of your run. nohup*.out is your bcl2fastq log file, '>' chooses a save location.
 nohup /usr/local/bin/bcl2fastq -R "$run" -o /git/nextseq/processed/"$dir_name" -r 16 -d 16 -p 16 -w "$sample_count" > /git/nextseq/processed/nohup_"$dir_name".out 2>&1 &
 
+else 
+
+echo Skipping demultiplexing. >> /git/nextseq/processed/"$dir_name".logfile;
+echo $d >> /git/nextseq/processed/"$dir_name".logfile;
+
+fi;
+
 #wait for demultiplex to finish before conversion.
 wait;
 ###COMMENT OUT IF YOU WANT TO ONLY DO CONVERSION######
 
+### MOVE REPORTS/ TO DROPBOX?
 
 #If the user does not want conversion and truncation of fastq.gz files.
 if [[ "$convert" == 0 ]]; then 
