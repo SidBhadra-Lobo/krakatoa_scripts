5efc49d7b0519940430d12036f4902d3c3d33d77
SidBhadra-Lobo <sbhadralobo@ucdavis.edu>
2016-10-26 23:24:36 -0400

    built around subroutines

--- nextseq_samplesheet_builder.pl
+++ nextseq_samplesheet_builder.pl
@@ -4,9 +4,11 @@
 
 use strict;
 use warnings;
-use diagnostics;
+# use diagnostics; #will also change "die" messages with "uncaught exception"
 
 use lib("/home/sbhadral");
+
+use Class::CSV;
 
 # open .csv table.
 open (my $fh_brcd, "<", "/home/sbhadral/barcode_tables/truseq_dna_meth_barcodes.csv")
@@ -16,7 +18,7 @@ my %barcodes; #initialize hash table
 while (my $line=<$fh_brcd>) { # read lines in
     if($line !~ /IndexName/) { #ignores the header
     chomp $line;
-    print $line,"\n";
+    # print $line,"\n";
 
     my ($index, $seq) = split /,/, $line; #split index and seq by ','
 
@@ -28,86 +30,114 @@ while (my $line=<$fh_brcd>) { # read lines in
         print "Key $key => $val\n"
     }
 
-#Function to check single barcodes entered.
+#Ask user what they want to do.
+print "Do you want to validate barcodes? Enter yes or no.\n";
+chomp(my $valid= <STDIN>);
+print "Do you want to build a samplesheet csv? Enter yes or no.\n";
+chomp(my $build= <STDIN>);
 
-# print "Do you want to check if a barcode sequence is valid?"
-print "Enter a barcode sequence to check : \n";
 
-chomp(my $brcd_seq = <STDIN>);
+if ( $valid =~ qw/yes/) {
+  &validator;
+}
 
-    # print "You entered $brcd_seq.\n";
+if ( $build=~ qw/yes/) {
+  &build_csv;
+}
 
-        my %revhash = reverse %barcodes; #get keys from value using reversed hash.
+else {die "You didn't choose yes to either option!";}
 
-    if( exists $revhash{$brcd_seq} ) { # check barcode give index if valid.
 
-        print "Your barcode sequence is valid.\n";
-        print "Here is the barcode index: ";
-        # my %revhash = reverse %barcodes;
-        my $key_check = $revhash{$brcd_seq};
-        print $key_check."\n"; 
+#Function to check single barcodes entered against a hash database.
+sub validator {
+
+  print "Enter a barcode sequence to check : \n";
+
+  chomp(my $brcd_seq = <STDIN>);
+
+      # print "You entered $brcd_seq.\n";
+
+          my %revhash = reverse %barcodes; #get keys from value using reversed hash.
+
+      if( exists $revhash{$brcd_seq} ) { # check barcode give index if valid.
+
+          print "Your barcode sequence is valid.\n";
+          print "Here is the barcode index: ";
+          # my %revhash = reverse %barcodes;
+          my $key_check = $revhash{$brcd_seq};
+          print $key_check."\n"; 
+      }
+      if(length($brcd_seq) < 6) {
+          print "Your barcode seems too short (less than 5 bp): $brcd_seq\n";
+      }
+      if($brcd_seq !~ /[ATCG]/) {
+          print "Your barcode seems to not be made of standard nucleotides: $brcd_seq\n";
+      }
+}
+
+#Build csv, with user input.
+sub build_csv {
+
+    print "Now, to build the sample sheet, default values are already set. \n";
+    NUM: while (1) {
+      print "Enter the run number as a single integer (ex. 4 or 5 ): \n";
+      chomp(my $run_num = <STDIN>);
+      
+      if ($run_num !~ qw/[0-9]{1}/) {
+            print "Please enter a valid run number. \n";
+            redo NUM;
+          }
+      last;    
     }
-    if(length($brcd_seq) < 6) {
-        print "Your barcode seems too short (less than 5 bp): $brcd_seq\n";
-    }
-    if($brcd_seq !~ /[ATCG]/) {
-        print "Your barcode seems to not be made of standard nucleotides: $brcd_seq\n";
-    }
+    print "Enter the date as mm/dd/yy (ex. 10/25/16) : \n";
+    chomp(my $date = <STDIN>);
+    print "Enter the run description delimited by underscores (ex. your_samp_description) : \n";
+    chomp(my $descp = <STDIN>);
+    print "Enter the read length for the forward read as an integer (ex. 151) : \n";
+    chomp(my $read1 = <STDIN>);
+    print "Enter the read length for the reverse read as an integer (ex. 0, if SingleEnd) : \n";
+    chomp(my $read2 = <STDIN>);
 
-#Start to build csv, with user input.
+      my $csv = Class::CSV->new(
+        fields => [qw/Sample_ID Sample_Name Sample_Plate Sample_Well I7_Index_ID index Sample_Project Description/] # This is for the [Data] section of the csv, even though the columns over lap with other sections.
+      );
 
-print "Now, to build the sample sheet, default values are already set : \n";
-print "Enter the run number as a single integer (ex. 4 or 5 ) : \n";
-chomp(my $run_num = <STDIN>);
-print "Enter the date as mm/dd/yy (ex. 10/25/16) : \n";
-chomp(my $date = <STDIN>);
-print "Enter the run description delimited by underscores (ex. your_samp_description) : \n";
-chomp(my $descp = <STDIN>);
-print "Enter the read length for the forward read as an integer (ex. 151) : \n";
-chomp(my $read1 = <STDIN>);
-print "Enter the read length for the reverse read as an integer (ex. 0, if SingleEnd) : \n";
-chomp(my $read2 = <STDIN>);
+      #Build header and settings section info.
+      $csv->add_line(['[Header]']);
+      $csv->add_line(['IEMFileVersion', 4]);
+      # $csv->add_line(['ExperimentName', "Run_$run_num"]); # Need to get rid of double quotes that show up on Experiment Name and pass variable properly.
+      $csv->add_line(['InvestigatorName', 'AnithaJayaprakash']);
+      $csv->add_line(['Date', "$date"]);
+      $csv->add_line(['Workflow', 'GenerateFASTQ']);
+      $csv->add_line(['Application', 'NextSeqFASTQOnly']);
+      $csv->add_line(['Assay', 'TruSeqHT']);
+      $csv->add_line(['Description', "$descp"]);
+      $csv->add_line(['Chemistry', 'Default']);
+      $csv->add_line(['']);
+      $csv->add_line(['[Reads]']);
+      $csv->add_line(["$read1"]);
+      $csv->add_line(["$read2"]);
+      $csv->add_line(['[Settings]']);
+      $csv->add_line(['']);
 
+      #Build data section.
+      $csv->add_line(['[Data]']);
+      $csv->add_line(['Sample_ID', 'Sample_Name', 'Sample_Plate', 'Sample_Well', 'I7_Index_ID', 'index', 'Sample_Project', 'Description']);
+      $csv->add_line({
+        Sample_ID => 'sampleblah',
+        Sample_Name => 'sample2name',
+        Sample_Plate => 'test',
+        Sample_Well => 'test',
+        I7_Index_ID => "A00what",
+        # index => "$brcd_seq",
+        index => "index#",
+        Sample_Project => 'sampleproject',
+        Description => "$descp"
+      });
 
-use Class::CSV;
+    $csv->print();
 
-my $csv = Class::CSV->new(
-  fields         => [qw/Sample_ID Sample_Name Sample_Plate Sample_Well I7_Index_ID index Sample_Project Description/] # This is for the [Data] section of the csv, even though the columns over lap with other sections.
-);
-
-#Build header and settings section info.
-$csv->add_line(['[Header]']);
-$csv->add_line(['IEMFileVersion', 4]);
-$csv->add_line(['ExperimentName', "Run_$run_num"]); # Need to get rid of double quotes that show up on Experiment Name.
-$csv->add_line(['InvestigatorName', 'AnithaJayaprakash']);
-$csv->add_line(['Date', "$date"]);
-$csv->add_line(['Workflow', 'GenerateFASTQ']);
-$csv->add_line(['Application', 'NextSeqFASTQOnly']);
-$csv->add_line(['Assay', 'TruSeqHT']);
-$csv->add_line(['Description', "$descp"]);
-$csv->add_line(['Chemistry', 'Default']);
-$csv->add_line(['']);
-$csv->add_line(['[Reads]']);
-$csv->add_line(["$read1"]);
-$csv->add_line(["$read2"]);
-$csv->add_line(['[Settings]']);
-$csv->add_line(['']);
-
-#Build data section.
-$csv->add_line(['[Data]']);
-$csv->add_line(['Sample_ID', 'Sample_Name', 'Sample_Plate', 'Sample_Well', 'I7_Index_ID', 'index', 'Sample_Project', 'Description']);
-$csv->add_line({
-  Sample_ID   => 'sampleblah',
-  Sample_Name => 'sample2name',
-  Sample_Plate => 'test',
-  Sample_Well => 'test',
-  I7_Index_ID => "A00what",
-  index => "$brcd_seq",
-  Sample_Project => 'sampleproject',
-  Description => "$descp"
-});
-
-$csv->print();
+}
 
 # Check for duplicate barcodes when reading in multiple barcodes from list.
 # my @keys = grep { $barcode{$_} eq $val } keys %barcodes; #check for duplicates
