daa311761c1cefd11100ee56b7893aa077c286d7
SidBhadra-Lobo <sbhadralobo@ucdavis.edu>
2016-10-31 10:35:57 -0700

    fixed duplicates

--- check_run_hole.sh
+++ check_run_hole.sh
@@ -4,19 +4,9 @@
 
 export RUNHOLE=`/bin/ps uax | grep ssh | grep -v uax | grep -v sshd | grep -v rsync | grep 2222`; 
 
-<<<<<<< HEAD
-if [[ ! $RUNHOLE ]]; then
-
-    echo hole is not running;
-    d=$(date);
-    echo $d;
-    /usr/bin/perl /home/ravi/run_hole.pl
-fi;
-=======
 if [[ ! "$RUNHOLE" ]]; then
     echo "port 2222 is not running";
     d=$(date);
     echo $d;
     /usr/bin/perl /home/ravi/run_hole.pl
 fi;
->>>>>>> 7e804b43a40cfa2c2dd7da3ebdd254bdbb2e9286
